66b4d08dc03b9a3e8e746f535030a7f8dc2b7e8c
Lj Miranda <ljvmiranda@gmail.com>
2022-04-21 06:02:50 +0800

    Fix test to run two workflows

--- test_ud_tagalog.py
+++ test_ud_tagalog.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+
 from spacy.cli.project.assets import project_assets
 from spacy.cli.project.run import project_run
 
@@ -8,4 +9,5 @@ from spacy.cli.project.run import project_run
 def test_ud_tagalog_project():
     root = Path(__file__).parent
     project_assets(root)
-    project_run(root, "all", capture=True)
+    project_run(root, "monolingual", capture=True)
+    project_run(root, "crosslingual", capture=True)
